12d2958cfc3303c7f38ffd4e19b8b2dee2bc9c23
Skakodub-K <skakodub03@bk.ru>
2025-02-09 21:33:06 +0300

    add items in dataSet

--- src/main.ts
+++ src/main.ts
@@ -12,24 +12,107 @@ async function main() {
 
     const dataSet = [
         {
-            inputPath: path.join(__dirname, '../A.png'),
+            inputPath: path.join(__dirname, '../alphabet/A.png'),
             correctAnswer: false
         },
         {
-            inputPath: path.join(__dirname, '../B.png'),
+            inputPath: path.join(__dirname, '../alphabet/B.png'),
             correctAnswer: true
         },
         {
-            inputPath: path.join(__dirname, '../C.png'),
+            inputPath: path.join(__dirname, '../alphabet/C.png'),
             correctAnswer: true
         },
         {
-            inputPath: path.join(__dirname, '../D.png'),
+            inputPath: path.join(__dirname, '../alphabet/D.png'),
             correctAnswer: true
         },
         {
-            inputPath: path.join(__dirname, '../E.png'),
+            inputPath: path.join(__dirname, '../alphabet/E.png'),
             correctAnswer: false
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/F.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/G.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/H.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/I.png'),
+            correctAnswer: false
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/J.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/K.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/L.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/M.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/N.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/O.png'),
+            correctAnswer: false
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/P.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/Q.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/R.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/S.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/T.png'),
+            correctAnswer: true
+        }, {
+            inputPath: path.join(__dirname, '../alphabet/U.png'),
+            correctAnswer: false
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/V.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/W.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/X.png'),
+            correctAnswer: true
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/Y.png'),
+            correctAnswer: false
+        },
+        {
+            inputPath: path.join(__dirname, '../alphabet/Z.png'),
+            correctAnswer: true
         }
     ]
 
