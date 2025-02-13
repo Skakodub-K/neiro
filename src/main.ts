4a2e8458d650ed94c625901e3f85fbf2d29f4f7b
Skakodub-K <skakodub03@bk.ru>
2025-02-13 13:08:04 +0300

    time counter

--- src/main.ts
+++ src/main.ts
@@ -1,132 +1,168 @@
 import path from "path";
-import {Neuro} from "./neuro";
-import {generateZones} from "./zoneGenerator"
+import { Neuro } from "./neuro";
+import { generateZones } from "./zoneGenerator";
+
+const rowsCount = 10;
+const columnCount = 6;
 
 async function main() {
-    
-    const rowsCount = 10;
-    const columnCount = 6;
-    
-    //Нейрон
-    const neuro = new Neuro(rowsCount * columnCount, 0);
+  //Нейрон
+  const neuro = new Neuro(rowsCount * columnCount, 0);
 
-    const dataSet = [
-        {
-            inputPath: path.join(__dirname, '../alphabet/A.png'),
-            correctAnswer: false
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/B.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/C.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/D.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/E.png'),
-            correctAnswer: false
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/F.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/G.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/H.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/I.png'),
-            correctAnswer: false
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/J.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/K.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/L.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/M.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/N.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/O.png'),
-            correctAnswer: false
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/P.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/Q.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/R.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/S.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/T.png'),
-            correctAnswer: true
-        }, {
-            inputPath: path.join(__dirname, '../alphabet/U.png'),
-            correctAnswer: false
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/V.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/W.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/X.png'),
-            correctAnswer: true
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/Y.png'),
-            correctAnswer: false
-        },
-        {
-            inputPath: path.join(__dirname, '../alphabet/Z.png'),
-            correctAnswer: true
-        }
-    ]
-
-    for (let data of dataSet) {
-        const dataSectors: Array<number> | undefined = await generateZones(data.inputPath, rowsCount, columnCount);
-        if (!dataSectors) {
-            console.log("Error!");
-            return;
-        }
-
-        while (neuro.getAnswer(dataSectors) === data.correctAnswer) {
-            neuro.adjustWeights(dataSectors, data.correctAnswer);
-        }
-        console.log("OK");
+  const dataSet = [
+    {
+      inputPath: path.join(__dirname, "../alphabet/A.png"),
+      correctAnswer: false,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/B.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/C.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/D.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/E.png"),
+      correctAnswer: false,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/F.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/G.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/H.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/I.png"),
+      correctAnswer: false,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/J.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/K.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/L.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/M.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/N.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/O.png"),
+      correctAnswer: false,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/P.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/Q.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/R.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/S.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/T.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/U.png"),
+      correctAnswer: false,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/V.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/W.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/X.png"),
+      correctAnswer: true,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/Y.png"),
+      correctAnswer: false,
+    },
+    {
+      inputPath: path.join(__dirname, "../alphabet/Z.png"),
+      correctAnswer: true,
+    },
+  ];
+  console.log(process.argv.length);
+  if (process.argv.length === 4) {
+    const dataSectors: Array<number> | undefined = await generateZones(
+      process.argv[3],
+      rowsCount,
+      columnCount
+    );
+    if (!dataSectors) {
+      console.log("Error!");
+      return;
     }
+    console.log((await neuro.getAnswer(dataSectors)) ? "гласная" : "согласная");
+    return;
+  } else {
+    await teach(neuro, dataSet);
+  }
 }
+
+async function teach(neuro: Neuro, dataSet: Array<any>) {
+  let countErrors: number = -1;
+  const start = new Date().getTime();
+  //Количество эпох
+  let countOfAges = 0;
+  
+  while (countErrors !== 0) {
+    countErrors = 0;
+    countOfAges++;
+    for (let data of dataSet) {
+      const dataSectors: Array<number> | undefined = await generateZones(
+        data.inputPath,
+        rowsCount,
+        columnCount
+      );
+      if (!dataSectors) {
+        console.log("Error!");
+        return;
+      }
+
+      while (neuro.getAnswer(dataSectors) === data.correctAnswer) {
+        countErrors++;
+        neuro.adjustWeights(dataSectors, data.correctAnswer);
+      }
+      console.log("OK");
+    }
+  }
+  const end = new Date().getTime();
+  console.log("Количество эпох: ", countOfAges);
+  console.log("Время обучения: ", end - start , "ms");
+}
+
 main();
